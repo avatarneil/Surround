20c0e9a383a7242d89c6b5cb982379f45e6fc54f
Neil Goldader <neilgoldader@longclaw.local>
2018-10-19 16:51:46 -0400

    Initial Commit

---
+++ health_kit_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  health_kit_test
+//
+//  Created by Neil Goldader on 10/19/18.
+//  Copyright © 2018 Neil Goldader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
